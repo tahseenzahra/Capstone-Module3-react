d7328a51fa491d28a56b608cd83f8821eabb0770
tahseenzahra <tahseen1zahra@gmail.com>
2022-11-29 11:08:36 +0500

    Router setup

--- src/App.js
+++ src/App.js
@@ -1,20 +1,13 @@
+import { Outlet } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <main>
+          <Outlet />
+        </main>
       </header>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -1,17 +1,39 @@
 import React from 'react';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Home from './pages/Homepg';
+import LandingPage from './pages/Landingpg';
+import Details from './pages/Detailpg';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: <LandingPage />,
+      },
+      {
+        path: 'dashboard',
+        element: <Home />,
+      },
+      {
+        path: 'details',
+        element: <Details />,
+      },
+    ],
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
