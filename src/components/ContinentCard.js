df187bf076eb09e5495ebede08f03d824ecef385
tahseenzahra <tahseen1zahra@gmail.com>
2022-12-01 18:29:34 +0500

    Display continent data on dashboard

--- src/components/ContinentCard.js
+++ src/components/ContinentCard.js
@@ -1,19 +1,40 @@
+/* eslint-disable no-param-reassign */
 import React from 'react';
 import { Link } from 'react-router-dom';
-import africaImg from '../assets/africa.png';
+import PropTypes from 'prop-types';
 
-const ContinentCard = () => (
-  <div className="continent-card">
-    <div className="continent-img">
-      <img src={africaImg} alt="Africa Map" />
+const ContinentCard = ({ continents, name, map }) => {
+  const TotalCases = () => {
+    if (continents.length) {
+      return continents.filter((country) => country.continent === name).reduce((acc, curr) => {
+        acc += curr.cases.total;
+        return acc;
+      }, 0);
+    }
+    return 0;
+  };
+
+  return (
+    <div className="continent-card">
+      <div className="continent-img">
+        <img src={map} alt="Africa Map" />
+      </div>
+      <h2 className="continent-name">{name}</h2>
+      <p className="continent-p">
+        <span>Total Cases</span>
+        <span style={{ fontSize: '1.2rem', fontWeight: 'bold' }}>{ TotalCases() }</span>
+      </p>
+      <Link to="/" className="continent-btn">
+        View Details
+      </Link>
     </div>
-    <h2 className="continent-name">Africa</h2>
-    <p>
-      <span>Total Cases: </span>
-      <span>323686</span>
-    </p>
-    <Link to="/" className="continent-btn">View Details</Link>
-  </div>
-);
+  );
+};
+
+ContinentCard.propTypes = {
+  continents: PropTypes.shape().isRequired,
+  name: PropTypes.string.isRequired,
+  map: PropTypes.string.isRequired,
+};
 
 export default ContinentCard;
--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,13 +1,19 @@
+/* eslint-disable */
+/* eslint-disable no-unused-expressions */
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import ContinentCard from '../components/ContinentCard';
 import { getAllData } from '../redux/world/worldReducer';
+import africaImg from '../assets/africa.png';
+import europeImg from '../assets/europe.png';
+import northImg from '../assets/north-america.png';
+import oceaniaImg from '../assets/australia.png';
+import southImg from '../assets/south-america.png';
+import asiaImg from '../assets/asia.png';
 
 const Dashboard = () => {
   const data = useSelector((state) => state.world);
   const dispatch = useDispatch();
-
-  // const Africa = data.filter((country) => country.continent === 'Africa');
 
   useEffect(() => {
     if (!data.length) {
@@ -15,16 +21,14 @@ const Dashboard = () => {
     }
   }, []);
 
-  console.log(data);
-
   return (
     <div className="dashboard">
-      <ContinentCard />
-      <ContinentCard />
-      <ContinentCard />
-      <ContinentCard />
-      <ContinentCard />
-      <ContinentCard />
+      <ContinentCard continents={data} name='Africa' map={africaImg} />
+      <ContinentCard continents={data} name='Europe' map={europeImg} />
+      <ContinentCard continents={data} name='North-America' map={northImg} />
+      <ContinentCard continents={data} name='Oceania' map={oceaniaImg} />
+      <ContinentCard continents={data} name='Asia' map={asiaImg} />
+      <ContinentCard continents={data} name='South-America' map={southImg} />
     </div>
   );
 };
