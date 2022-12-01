69e254374f37b586bd8a5655df448ccc53ab99cc
tahseenzahra <tahseen1zahra@gmail.com>
2022-12-01 14:27:27 +0500

    Setup route for Dashboard

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+import { Outlet } from 'react-router-dom';
+
 function App() {
-  return <div className="App">App</div>;
+  return (
+    <div className="App">
+      <main>
+        <Outlet />
+      </main>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/pages/Dashboard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Dashboard = () => {
+  const title = 'Dashboard';
+  return (
+    <div>
+      <h2>{title}</h2>
+    </div>
+  );
+};
+
+export default Dashboard;
