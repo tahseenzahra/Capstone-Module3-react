3f3d4e95ce90d93de0e3a21f27d44fbb738997b2
tahseenzahra <tahseen1zahra@gmail.com>
2022-12-01 15:43:52 +0500

    Dispatch action to get data in Dashboard.js

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,7 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getAllData } from '../redux/world/worldReducer';
 
 const Dashboard = () => {
   const title = 'Dashboard';
+  const data = useSelector((state) => state.world);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (!data.length) {
+      dispatch(getAllData());
+    }
+  }, []);
+
+  console.log(data);
+
   return (
     <div>
       <h2>{title}</h2>
--- src/redux/api.js
+++ src/redux/api.js
@@ -1,18 +1,18 @@
 import axios from 'axios';
 
-export const URL = 'https://vaccovid-coronavirus-vaccine-and-treatment-tracker.p.rapidapi.com/api/npm-covid-data/world';
+export const URL = 'https://covid-193.p.rapidapi.com/statistics';
 
 export const headers = {
-  'X-RapidAPI-Key': '289eb98e74msha2f6cdf72681c12p1b70d6jsn7404c93fcd81',
+  'X-RapidAPI-Key': 'fbdb1a7eb1msh0ca64088d4cd139p1264d2jsn3ab8096a4d8c',
   'X-RapidAPI-Host':
-    'vaccovid-coronavirus-vaccine-and-treatment-tracker.p.rapidapi.com',
+    'covid-193.p.rapidapi.com',
 };
 
 const fetchData = async () => {
   const response = await axios.get(URL, {
     headers,
   });
-  return response.data;
+  return response.data.response;
 };
 
 export default fetchData;
