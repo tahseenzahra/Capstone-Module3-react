92683933b2349b9e915320c1813cb44b7d4cc218
tahseenzahra <tahseen1zahra@gmail.com>
2022-12-01 13:41:51 +0500

    Project setup

--- src/App.js
+++ src/App.js
@@ -1,22 +1,5 @@
-import { Outlet, useLocation } from 'react-router-dom';
-import { NavBar } from './components';
-
 function App() {
-  const location = useLocation();
-  return (
-    <div className="App md:flex md:relative">
-      {location.pathname !== '/' ? <NavBar /> : null}
-      {location.pathname === '/' ? (
-        <main className="w-full">
-          <Outlet />
-        </main>
-      ) : (
-        <main className="md:w-[70%] xl:w-[80%] md:ml-[30%] xl:ml-[20%]">
-          <Outlet />
-        </main>
-      )}
-    </div>
-  );
+  return <div className="App">App</div>;
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,42 +1,13 @@
 import React from 'react';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import { Provider } from 'react-redux';
-import store from './redux/configurestore';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Home from './pages/Homepg';
-import LandingPage from './pages/Landingpg';
-import { Details } from './components';
-
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <App />,
-    children: [
-      {
-        index: true,
-        element: <LandingPage />,
-      },
-      {
-        path: 'dashboard',
-        element: <Home />,
-      },
-      {
-        path: 'details',
-        element: <Details />,
-      },
-    ],
-  },
-]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <RouterProvider router={router} />
-    </Provider>
+    <App />
   </React.StrictMode>,
 );
 
